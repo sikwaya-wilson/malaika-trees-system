368ad05ba50b868a6fedf10fa97edb5e0a651fb4
sikwaya <sikwayamumbere@gmail.com>
2025-11-30 12:08:50 +0300

    Update database configuration

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "malaika_ranch";
+$username = "root";
+$password = "";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die("DB Connection failed: " . $e->getMessage());
+}
+?>
